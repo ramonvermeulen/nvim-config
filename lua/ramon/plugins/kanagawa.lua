19c0b2a65ee4dbb5c673d999218e2a1945fd2fe8
ramonvermeulen <ramonvermeulen98@gmail.com>
2024-10-14 10:08:11 +0200

    feat: update theme

--- lua/ramon/plugins/kanagawa.lua
+++ lua/ramon/plugins/kanagawa.lua
@@ -3,12 +3,7 @@ return {
 		"rebelot/kanagawa.nvim",
 		name = "kanagawa",
 		priority = 1000,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
 		config = function()
-			-- Default options:
 			require("kanagawa").setup({
 				compile = false, -- enable compiling the colorscheme
 				undercurl = true, -- enable undercurls
@@ -20,22 +15,11 @@ return {
 				transparent = false, -- do not set background color
 				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				colors = { -- add/modify theme and palette colors
-					palette = {},
-					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-				},
-				overrides = function(colors) -- add/modify highlights
-					return {}
-				end,
-				theme = "wave", -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = "wave", -- try "dragon" !
-					light = "lotus",
-				},
+				theme = "dragon", -- Load "wave" theme when 'background' option is not set
 			})
 
 			-- setup must be called before loading
-			vim.cmd("colorscheme kanagawa dragon")
+			vim.cmd("colorscheme kanagawa-dragon")
 		end,
 	},
 }
